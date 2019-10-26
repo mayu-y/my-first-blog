7891b60f918032fb2ad92a72e3ad16f8d0188f4f
Your Name <you@example.com>
2019-10-26 16:18:34 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.shortcuts import render, get_object_or_404
+
 urlpatterns = [
-    path('', views.post_list, name=' st'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
-from django.shortcuts import render # Create your views here.
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
