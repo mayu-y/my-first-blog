15a2b9902af7979fcd87161e2c6537a3457e82ec
Your Name <you@example.com>
2019-10-26 13:09:58 +0900

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -12,6 +13,6 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    
+
     def __str__(self):
         return self.title
